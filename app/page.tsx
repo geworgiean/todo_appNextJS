1e3ec0f0c94851fe386f2695dc9af0e50f98e1be
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-06 21:05:40 +0400

    aded archive section

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,20 @@ import { Task } from './types/task'
 export default function Home() {
   const [todos, setTodos] = useState<Task[]>([]);
   const [todo, setTodo] = useState<string>("");
+  const [archive, setArchive] = useState<Task[]>([]);
 
+  
   useEffect(() => {
     const savedTodos = localStorage.getItem("todos");
-    if (savedTodos) {
-      setTodos(JSON.parse(savedTodos));
-    }
+    const savedArchive = localStorage.getItem("archive");
+    if (savedTodos) setTodos(JSON.parse(savedTodos));
+    if (savedArchive) setArchive(JSON.parse(savedArchive));
   }, []);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+    localStorage.setItem("archive", JSON.stringify(archive));
+  }, [todos, archive]);
 
   const updateToDoList = () => {
     if (todo.trim()) {
@@ -36,12 +39,19 @@ export default function Home() {
   const [dueDate, setDueDate] = useState<string>("");
 
   const toggleTodo = (id: string) => {
-    setTodos(
-      todos.map((task) =>
-      task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
-      )
-    );
-  };
+  setTodos((prev) => {
+    return prev.filter((task) => {
+      if (task.id === id) {
+        const updated = { ...task, isCompleted: !task.isCompleted };
+        if (updated.isCompleted) {
+          setArchive([...archive, updated]);  
+          return false;  
+        }
+      }
+      return true;
+    });
+  });
+};
 
   const deleteTodo = (id: string) => {
     setTodos(todos.filter((task) => task.id !== id));
@@ -71,7 +81,6 @@ export default function Home() {
                        Add
           </button>
         </div>
-
         <ul className="space-y-2">
           {todos.map((task) => (
             <li
@@ -94,6 +103,23 @@ export default function Home() {
               <button
                 onClick={() => deleteTodo(task.id)}
                 className="text-red-500 hover:text-red-900">
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+        <h2 className="text-2xl font-bold text-[#4B2E20] mt-8 mb-4">Archive</h2>
+        <ul className="space-y-2">
+          {archive.map((task) => (
+            <li 
+            key={task.id}
+            className="flex justify-between items-center bg-gray-300 px-3 py-2 rounded opacity-70"
+            >
+              <span className="line-through font-bold text-[#4B2E20]">{task.title}</span>
+              <button 
+                onClick={() => setArchive(archive.filter((t) => t.id !== task.id))}
+                className="text-red-600 hover:text-red-900"
+              >
                 Delete
               </button>
             </li>
